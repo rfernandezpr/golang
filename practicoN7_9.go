4bf8d69f41dc4e70c7badf0ef49e7d3bff00316d
rafb712 <rogerfernandez.pr@gmail.com>
2021-10-14 14:11:59 -0300

    Seccion 7 ejercicio # 9.1

--- practicoN7_9.go
+++ practicoN7_9.go
@@ -10,11 +10,11 @@ func main() {
 
 	switch deporteFav {
 	case "futboll":
-		fmt.Println("Su deporte favorito es el %v", deporteFav)
+		fmt.Printf("Su deporte favorito es el %v", deporteFav)
 	case "basketboll":
-		fmt.Println("Su deporte favorito es el %v", deporteFav)
+		fmt.Printf("Su deporte favorito es el %v", deporteFav)
 	case "baseball":
-		fmt.Println("Su deporte favorito es el %v", deporteFav)
+		fmt.Printf("Su deporte favorito es el %v", deporteFav)
 	default:
 		fmt.Println("No tiene deporte favorito")
 	}
